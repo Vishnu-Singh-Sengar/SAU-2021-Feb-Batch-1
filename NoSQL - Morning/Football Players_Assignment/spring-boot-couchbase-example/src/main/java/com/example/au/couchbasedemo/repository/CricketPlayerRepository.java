3faf4e11727c751e6ab96908a9f331cb144d82da
Vishnu-Singh-Sengar <vishnu.sengar@accolitedigital.com>
2021-02-17 15:10:09 +0530

    Added compressed codes into single file

--- NoSQL - Morning/Football Players_Assignment/spring-boot-couchbase-example/src/main/java/com/example/au/couchbasedemo/repository/CricketPlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.au.couchbasedemo.repository;
-
-import com.example.au.couchbasedemo.model.CricketPlayers;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CricketPlayerRepository extends CrudRepository<CricketPlayers, String> {
-
-
-}
