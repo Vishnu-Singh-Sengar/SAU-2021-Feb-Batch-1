ccd14436689d4ab9842d1a63615811c10fb51bf6
Vishnu-Singh-Sengar <vishnu.sengar@accolitedigital.com>
2021-03-02 20:24:03 +0530

    Completed Q2. Spring Assignment Completegit add .git add .

---
+++ Spring Framework Core, AOP - Morning/Question 2/Code/src/com/au/aop/model/Triangle.java
@@ -0,0 +1,15 @@
+package com.au.aop.model;
+
+public class Triangle {
+	
+	private String Name;
+
+	public String getName(){
+		return Name;
+	}
+
+	public void setName(String name) {
+		this.Name = name;
+	}
+
+}
